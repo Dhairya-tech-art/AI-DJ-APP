528b0233d695917ceb46eae6defbb26315cca041
Dhairya-tech-art <74977651+Dhairya-tech-art@users.noreply.github.com>
2021-06-18 18:48:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+scoreLeftWrist=0;
 function preload()
 {
     song=loadSound("music.mp3");
@@ -24,6 +25,17 @@ function setup() {
 }
 function draw() {
     image(video, 0, 0, 600, 500)
+
+    fill("#FF0000");
+    stroke("FF0000");
+
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY = Number(leftWristY)
+    remove_decimals = floor(InNumberleftWristY);
+    leftWristY_divided_1000= remove_decimals/1000;
+    volume = leftWristY_divided_1000 *2;
+    document.getElementById("volume").innerHTML="Volume="+ volume;
+    song.setVolume(volume);
 }
 
 function play()
@@ -37,6 +49,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist= results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist=" + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = "+leftWristX +" leftWristY = " + leftWristY);
